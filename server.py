f1433e99a08e21acf97d4957b4d3405458d7d74c
fis007 <schaffhauserfilip@gmail.com>
2020-10-17 21:11:05 +0800

    added form to database

--- server.py
+++ server.py
@@ -1,8 +1,12 @@
+import os
+import peeweedbevolve
 import peeweedbevolve  # new; must be imported before models
-from flask import Flask, render_template, request
-from models import db
+from flask import Flask, render_template, request, flash, redirect, url_for
+from models import db, Store
+
 
 app = Flask(__name__)
+app.secret_key = os.getenv('SECRET_KEY')
 
 
 @app.before_request
@@ -24,6 +28,23 @@ def migrate():  # new
 @app.route("/")
 def index():
     return render_template('index.html')
+
+
+@app.route("/store/")
+def store():
+    return render_template('store.html')
+
+
+@app.route("/store_form")
+def store_form():
+    s = Store(name=request.args['name'])
+
+    if s.save():
+        flash("Successfully saved", "success")
+        return redirect(url_for('store'))
+    else:
+        flash("Unable to create!", "danger")
+        return render_template('store.html', name=request.args['name'])
 
 
 if __name__ == '__main__':
